382f0e1121379cae146c13c15518ebffbebd2c42
wiseguy16 <gergusa04@gmail.com>
2016-08-02 13:33:03 -0400

    Initial Commit

---
+++ InDueTime/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  InDueTime
+//
+//  Created by Gregory Weiss on 8/2/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ InDueTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InDueTime
+//
+//  Created by Gregory Weiss on 8/2/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
